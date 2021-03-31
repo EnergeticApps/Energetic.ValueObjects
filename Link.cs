d7a062c47a2ac12586ed41e31239a00981b2e9ba
Benjamin <benjaminjcharlton@gmail.com>
2021-03-31 17:33:22 +0400

    Made a nice value object for hyperlinks

---
+++ Link.cs
@@ -0,0 +1,36 @@
+﻿namespace Energetic.ValueObjects
+{
+    public record Link : ValueObject<Link, (string, string, Url)>
+    {
+        public Link(string text, string title, Url url) : base((text, title, url))
+        {
+        }
+
+        public string Text
+        {
+            get
+            {
+                var (text, _, _) = Value;
+                return text;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                var (_, title, _) = Value;
+                return title;
+            }
+        }
+
+        public Url Url
+        {
+            get
+            {
+                var (_, _, url) = Value;
+                return url;
+            }
+        }
+    }
+}
